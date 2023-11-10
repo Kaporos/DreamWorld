7b8080e0eb15efd4011ffb19ba09714419ffe350
Kaporos <theo@daron.be>
2023-11-10 18:55:29 +0100

    adding base movement

---
+++ Player.gd
@@ -0,0 +1,33 @@
+extends CharacterBody2D
+
+
+@export var SPEED = 800.0
+@export var JUMP_VELOCITY = -800.0
+@export var DECELERATION = 1000;
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = 800*2
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	if is_on_floor():
+		velocity.y += JUMP_VELOCITY;
+	
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction != 0:
+		velocity.x = SPEED * direction ;
+	
+	if velocity.x > 0: 
+		if velocity.x - DECELERATION*delta < 0:
+			velocity.x = 0
+		else:
+			velocity.x -= DECELERATION * delta;
+	if velocity.x < 0: 
+		if velocity.x + DECELERATION*delta > 0:
+			velocity.x = 0
+		else:
+			velocity.x += DECELERATION * delta;
+
+	move_and_slide()
