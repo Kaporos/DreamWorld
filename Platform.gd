24f2834124045ddb52291ba030f4c89b916e34fb
Kaporos <theo@daron.be>
2023-11-11 11:35:18 +0100

    fixing platforms

--- Platform.gd
+++ Platform.gd
@@ -1,16 +1,11 @@
 extends StaticBody2D
 
 var appeared = false;
-var player;
+var marker;
 var rng = RandomNumberGenerator.new()
 func _ready():
-	player = get_tree().get_nodes_in_group("player")[0]
-func _on_visible_on_screen_notifier_2d_screen_entered():
-	appeared = true;
-
-
-func _on_visible_on_screen_notifier_2d_screen_exited():
-	if appeared:
-		position.y += (player.position.y - position.y - rng.randf_range(0, 100.0) )
-		
-		print(position.y)
+	var player = get_tree().get_nodes_in_group("player")[0]
+	marker = player.get_node("Marker")
+func _process(_delta):
+	if global_position.y > marker.global_position.y:
+		queue_free()
